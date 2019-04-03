aae9498250244450b4552e115c15aa934b1a0861
Kevin Garvey and Zema Kebede <kevin@>
2019-04-03 10:51:19 -0700

    "Identify single vowels and add "-ay" to the end"

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 function abcCheck(word) {
   var letters = /^[a-zA-Z]+$/.test(word);
   if (letters === true) {
-    alert("true");
+    alert(word)
+    if (word === "a" || word === "e" || word === "i" || word === "o" || word === "u") {
+      word = word.concat("ay");
+      alert (word)
+    } else if (word !== "a"){
+      alert("not a vowel")
+    }
+
   } else {
     alert("Please enter letters only");
+    return word
   }
 }
 
@@ -17,6 +25,7 @@ $(document).ready(function(){
     event.preventDefault();
     var word = $("input#wordToTranslate").val();
     var letters= abcCheck(word);
+    //alert(word);
     // if (letters === true) {
     //   alert("letter")
     // } else {
