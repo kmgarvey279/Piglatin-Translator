f280365d2fe14e002e8a4a2a16c402ee7fee5201
Kevin Garvey and Zema Kebede <kevin@>
2019-04-03 10:18:04 -0700

    add check for letters

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 function abcCheck(word) {
   var letters = /^[a-zA-Z]+$/.test(word);
-  alert(letters);
-  alert(word);
-  // if word.includes(letters) {
-  //   return true
-  // }
-  // else{
-  //   return false
-  // }
+  if (letters === true) {
+    alert("true");
+  } else {
+    alert("Please enter letters only");
+  }
 }
 
 
@@ -19,12 +16,12 @@ $(document).ready(function(){
   $("form#pigLatin").submit(function(event){
     event.preventDefault();
     var word = $("input#wordToTranslate").val();
-    var stringIsABC = abcCheck(word);
-    if (stringIsABC === true) {
-      alert("letter")
-    } else {
-      alert("not a letter")
-    }
+    var letters= abcCheck(word);
+    // if (letters === true) {
+    //   alert("letter")
+    // } else {
+    //   alert("not a letter")
+    // }
 
     //  $("#convertedWord").append(word);
   });
